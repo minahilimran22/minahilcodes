0940486352b70899fc396353a521c55d2e8832f5
minahilimran22 <minahilimranmuhammadimran514@gmail.com>
2026-02-22 09:54:05 -0800

    message

---
+++ prime.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main(){
+
+
+cout<<"   -------------------------           "<<endl;
+cout<<"                                          "<<endl;
+cout<<"     o   ^__^                            "<<endl;
+cout<<"      o  (oo)\\___________                    "<<endl;
+cout<<"         (oo)\\           )\\/\\          "<<endl;
+cout<<"             ||-------w  |              "<<endl;
+cout<<"             ||         ||              "<<endl;
+}
---
+++ zero.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main(){  
+
+cout<<"   +--^----------,--------,-----,--------^-,    "<<endl;
+cout<<"    | |||||||||   '--------'     |          0  "<<endl;
+cout<<"    '+---------------------------^----------|   "<<endl;
+cout<<"      ',---------,---------,--------------,    "<<endl;
+cout<<"        / XXXXXX /'|       /'                    "<<endl;
+cout<<"       / XXXXXX /  |      /'                     "<<endl;
+cout<<"      / XXXXXX /'-------'             "<<endl;
+cout<<"     / XXXXXX /            "<<endl;
+cout<<"    / XXXXXX /          "<<endl;
+cout<<"   (--------(             "<<endl;
+cout<<"    '------'               "<<endl;
+
+}
+   
